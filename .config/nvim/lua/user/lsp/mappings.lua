c411fda0baa0244b762fa679752947087edb9363
ngladkikh <ngladkikh@adsterra.com>
2023-03-19 14:14:19 +0500

    feat(nvim): remove config

--- .config/nvim/lua/user/lsp/mappings.lua
+++
@@ -1,6 +0,0 @@
--- easily add or disable built in mappings added during LSP attaching
-return {
-  n = {
-    -- ["<C-i>"] = { function() vim.lsp.buf.signature_help() end, desc = "Signature help" }
-  },
-}
